2c7b76af49736ce6ec9151fd2dd9085dbb5bace5
Ashish Tiwari <ashishtiwari1314q@gmail.com>
2026-08-10 17:50:16 +0530

    Setup of .env file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Start of backen project");
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: "./.env",
+})
+
+let  myusername = process.env.MY_USERNAME;
+let  mydatabase = process.env.database;
+
+
+
+console.log("value:", myusername);
+console.log("value:", mydatabase);
+
+console.log("Start of backend project");

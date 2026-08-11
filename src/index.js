8fa7f8c9be1d305e872f7289dc68942bafef5ec0
Ashish Tiwari <ashishtiwari1314q@gmail.com>
2026-08-11 17:44:53 +0530

    MongoDB Connected with node.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import app from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+})
+
+
+const port = process.env.PORT || 3000;
+
+
+connectDB().then(()=>{
+    app.listen(port, () => {
+  console.log(`Your server are runing on port http://localhost:${port}`)
+})
+}).catch((err)=>{
+  console.log("MongoDB are not connected ", err)
+  process.exit(1)
+})
